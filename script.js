99f1439113f18634fc483149264c17aac6cec56d
Karim Bajeddi <125909979+Seewhy22@users.noreply.github.com>
2025-06-24 03:40:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analyst", "Suppy Chain Analyst", "Business Analyst ", "Data Manager", "Student"],
+        strings: ["Data Analyst", "Student", "Business Analyst ", "Data Manager", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Analyst", "Suppy Chain Analyst", "Business Analyst ", "Data Manager", "Student"],
+        strings: ["Data Analyst", "Student", "Business Analyst ", "Data Manager", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
